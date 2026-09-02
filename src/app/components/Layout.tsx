5341cb3fba02e8e9382af4a5e7e0af83d3a0d514
ahmedmamdouh120456-source <ahmed.mamdouh120456@gmail.com>
2026-09-02 21:27:22 +0300

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -7,8 +7,7 @@ import { LangProvider, useLang } from "./LanguageContext";
 function LayoutInner() {
   const { dir } = useLang();
   return (
-    <div className="min-h-screen flex flex-col" dir={dir} style={{ background: "#1A0E1E", fontFamily: "'Cairo', sans-serif" }}>
-      <Navbar />
+<div className="min-h-screen w-full overflow-x-hidden flex flex-col"      <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
